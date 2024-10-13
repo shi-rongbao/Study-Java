c40eb8958302d6a460e3a6e1121ba12cfe6d3626
ShiRongbao <7447664226srb@gmail.com>
2024-10-13 23:28:57 +0800

    maven-test code

---
+++ maven_test/maven_java/src/main/java/com/shirongbao/maven/pojo/User.java
@@ -0,0 +1,16 @@
+package com.shirongbao.maven.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    private Integer id;
+    
+    private String username;
+
+    private String password;
+}
---
+++ maven_test/maven_web/src/main/java/com/shirongbao/maven/pojo/Student.java
@@ -0,0 +1,7 @@
+package com.shirongbao.maven.pojo;
+
+public class Student {
+    public void sayHello(){
+
+    }
+}
