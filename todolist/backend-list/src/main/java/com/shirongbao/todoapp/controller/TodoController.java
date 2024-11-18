71824a6f40f3f1253d08e0ea52c15437a4841a8d
ShiRongbao <7447664226srb@gmail.com>
2024-11-18 15:48:16 +0800

    修改跨域配置

--- todolist/backend-list/src/main/java/com/shirongbao/todoapp/controller/TodoController.java
+++ todolist/backend-list/src/main/java/com/shirongbao/todoapp/controller/TodoController.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/todos")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 public class TodoController {
 
     @Resource
